9e1745a6a2b94f2e57c783452822b7ac1a53eaee
Tshireletso Selemela <159533600+Stewiecancode@users.noreply.github.com>
2026-05-10 21:03:40 +0200

    Update Index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import { Server } from "@modelcontextprotocol/sdk/server/index.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
+
+const server = new Server({
+  name: "MCP Server",
+  version: "1.0.0",
+}, {
+capabilities: {
+    resources: {},
+    tools:{}
+}
+})
